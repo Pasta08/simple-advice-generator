4371dbc3f0eb318457b0c590bb3935609685a35d
Ethan Bone Ortega <ethanbone435@gmail.com>
2022-08-24 13:17:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './style/App.css';
 
 function App() {
-  return (
+  const [ advice, setAdvice ] = useState()
+  const [ loading, setLoading ] = useState(true)
+  
+  const getAdvice = async() =>{
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data)
+    setAdvice(data)
+    setLoading(false)
+  }
+  useEffect(()=>{
+    getAdvice()
+  }, [])
+
+  if( loading === false ){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='container'>
+            <span>{`ADVICE #${advice.slip.id}`}</span> 
+            <p>{`"${advice.slip.advice}"`}</p>
+            <img src='./images/pattern-divider-desktop.svg'/>
+            <button onClick={getAdvice}>
+              <img src='./images/icon-dice.svg'/>
+            </button>
+        </div>
     </div>
   );
+  }
+  
 }
 
 export default App;
